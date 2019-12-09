07778130be113b872296125b6c4c0900bb509777
Naat Hybur <nataliemhruby@gmail.com>
2019-12-09 02:46:38 -0700

    removed functiosn to remove function call overhead

--- Day_9/main.py
+++ Day_9/main.py
@@ -4,8 +4,8 @@ RELATIVE = 2
 
 ADD = 1
 MULT = 2
-IN = 3
-OUT = 4
+INPUT = 3
+OUTPUT = 4
 JUMP_IF_NOT_ZERO = 5
 JUMP_IF_ZERO = 6
 LESS_THAN = 7
@@ -16,11 +16,12 @@ HALT = 99
 READ = 0
 WRITE = 1
 
+#Dictionary of permissions each value has per operation
 OPS = {
     ADD: (READ, READ, WRITE),
     MULT: (READ, READ, WRITE),
-    IN: (WRITE,),
-    OUT: (READ,),
+    INPUT: (WRITE,),
+    OUTPUT: (READ,),
     JUMP_IF_NOT_ZERO: (READ, READ),
     JUMP_IF_ZERO: (READ, READ),
     LESS_THAN: (READ, READ, WRITE),
@@ -62,79 +63,33 @@ class Computer():
 
             if op_code == HALT:
                 self.halted = True
-                self.op_code_99()
 
             if op_code == ADD:
-                self.op_code_1(value_1, value_2, value_3)
+                self.data[value_3] = value_1 + value_2
 
             elif op_code == MULT:
-                self.op_code_2(value_1, value_2, value_3)
+                self.data[value_3] = value_1 * value_2
 
-            elif op_code == IN:
-                self.op_code_3(value_1)
+            elif op_code == INPUT:
+                self.data[value_1] = self.inputs.pop(0)
 
-            elif op_code == OUT:
-                self.op_code_4(value_1)
+            elif op_code == OUTPUT:
+                self.add_output(value_1)
 
             elif op_code == JUMP_IF_NOT_ZERO:                
-                self.op_code_5(value_1, value_2)
+                self.pointer = value_2 if value_1 != 0 else self.pointer
 
             elif op_code == JUMP_IF_ZERO:
-                self.op_code_6(value_1, value_2)
+                self.pointer = value_2 if value_1 == 0 else self.pointer
 
             elif op_code == LESS_THAN:
-                self.op_code_7(value_1, value_2, value_3)
+                self.data[value_3] = 1 if value_1 < value_2 else 0
 
             elif op_code == EQUALS:
-                self.op_code_8(value_1, value_2, value_3)
+                self.data[value_3] = 1 if value_1 == value_2 else 0
 
             elif op_code == ADD_RELATIVE_BASE:
-                self.op_code_9(value_1)
-
-    def op_code_1(self, value_1, value_2, value_3):
-        self.data[value_3] = value_1 + value_2
-
-    def op_code_2(self, value_1, value_2, value_3):
-        self.data[value_3] = value_1 * value_2
-
-    def op_code_3(self, value_1):
-
-        self.data[value_1] = self.inputs.pop(0)
-
-    def op_code_4(self, value_1):
-
-        self.add_output(value_1)
-
-    def op_code_5(self, value_1, value_2):
-
-        if value_1 != 0:
-            self.pointer = value_2
-
-    def op_code_6(self, value_1, value_2):
-
-        if value_1 == 0:
-            self.pointer = value_2
-
-    def op_code_7(self, value_1, value_2, value_3):
-
-        if value_1 < value_2:
-            self.data[value_3] = 1
-        else:
-            self.data[value_3] = 0
-
-    def op_code_8(self, value_1, value_2, value_3):
-
-        if value_1 == value_2:
-            self.data[value_3] = 1
-        else:
-            self.data[value_3] = 0
-
-    def op_code_9(self, value_1):
-
-        self.relative_base += value_1
-
-    def op_code_99(self):
-        self.halted = True
+                self.relative_base += value_1
 
     def add_input(self, input):
         self.inputs.append(input)
@@ -149,23 +104,26 @@ class Computer():
         args = [None] * 4
 
         for i, kind in enumerate(arg_kinds):
-            data_reference = self.data[self.pointer + 1 + i]
+            data_reference_pointer = self.data[self.pointer + 1 + i]
             mode = modes % 10
             modes //= 10
 
             if mode == RELATIVE:
-                data_reference += self.relative_base
+                data_reference_pointer += self.relative_base
 
             if mode in (POSITION, RELATIVE):
-                self.resize_memory(data_reference)
+                self.resize_memory(data_reference_pointer)
 
-                if data_reference < 0:
-                    raise Exception(f"Invalid access to negative memory index: {data_reference}")
-                elif data_reference >= len(self.data):
-                    self.data += [0] * (data_reference + 1 - len(self.data))
+                #if data_reference_pointer references a negative address, throw an exception
+                #if data_reference_pointer references an address outside current memory, add enough memory
+                #so it can reference it and fill with 0s
+                if data_reference_pointer < 0:
+                    raise Exception(f"Invalid access to negative memory index: {data_reference_pointer}")
+                elif data_reference_pointer >= len(self.data):
+                    self.data += [0] * (data_reference_pointer + 1 - len(self.data))
 
                 if kind == READ:
-                    data_reference = self.data[data_reference]
+                    data_reference_pointer = self.data[data_reference_pointer]
                 elif kind != WRITE:
                     raise Exception(f"Invalid arg kind: {kind}")
 
@@ -175,7 +133,7 @@ class Computer():
             else:
                 raise Exception(f"Invalid arg mode: {mode}")
 
-            args[i] = data_reference
+            args[i] = data_reference_pointer
 
         return args
 
